144b85e77a7f282dcb47134c70e51786076c5681
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-05-01 09:34:56 +0600

    inital

---
+++ src/pages/Auth/Register/Register.jsx
@@ -0,0 +1,122 @@
+import toast from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { axiosFetch, generateImageURL } from '../../../utils';
+import './Register.scss'
+
+const Register = () => {
+  const navigate = useNavigate();
+  const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [formInput, setFormInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone: '',
+    description: '',
+    isSeller: false,
+  });
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    for (let key in formInput) {
+      if (formInput[key] === '') {
+        toast.error('Please fill all input field: ' + key);
+        return;
+      }
+      else if (key === 'phone' && formInput[key].length < 9) {
+        toast.error('Enter valid phone number!');
+        return;
+      }
+    }
+
+    setLoading(true);
+
+    // const { url } = await generateImageURL(image);
+
+    try {
+
+      const { data } = await axiosFetch.post('/auth/register', { ...formInput, image: 'https://res.cloudinary.com/dcdgqiusj/image/upload/v1682917998/s5rykmglenf5rqwqdpfe.jpg' });
+      toast.success('Registration successful!');
+      setLoading(false);
+      navigate('/login');
+    }
+    catch ({ response }) {
+      console.log(response)
+      toast.error(response?.data?.message);
+      setLoading(false);
+    }
+  }
+
+  const handleChange = (event) => {
+    const { value, name, type, checked } = event.target;
+    const inputValue = type === 'checkbox' ? checked : value;
+    setFormInput({
+      ...formInput,
+      [name]: inputValue
+    });
+  }
+
+  return (
+    <div className="register">
+      <form onSubmit={handleSubmit}>
+        <div className="left">
+          <h1>Create a new account</h1>
+          <label htmlFor="">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="johndoe"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Password</label>
+          <input name="password" type="password" onChange={handleChange} />
+          <label htmlFor="">Profile Picture</label>
+          <input type="file" onChange={(event) => setImage(event.target.files[0])} />
+          <button type="submit" disabled={loading}>{loading ? 'Loading...' : 'Register'}</button>
+        </div>
+        <div className="right">
+          <p>Already have an account? <Link to='/login'>Signin</Link></p>
+          <h1>I want to become a seller</h1>
+          <div className="toggle">
+            <label htmlFor="">Activate the seller account</label>
+            <label className="switch">
+              <input type="checkbox" name='isSeller' onChange={handleChange} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <label htmlFor="">Phone Number</label>
+          <input
+            name="phone"
+            type="text"
+            placeholder="+1 1234 567 890"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Description</label>
+          <textarea
+            placeholder="A short description of yourself"
+            name="description"
+            id=""
+            cols="30"
+            rows="10"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Register
