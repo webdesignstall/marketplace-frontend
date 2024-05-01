a060dd8dd7f028f252eb1d50868b3a9af9f4ac56
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-05-01 10:23:50 +0600

    [Done] cloudinary

--- src/utils/generateImageURL.js
+++ src/utils/generateImageURL.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const generateImageURL = async (image) => {
   const file = new FormData();
   file.append("file", image);
-  file.append("upload_preset", process.env.CLOUDINARY_PRESET);
+  file.append("upload_preset", import.meta.env.VITE_CLOUDINARY_PRESET);
 
   const { data } = await axios.post(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_ENV}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_ENV}/image/upload`,
     file
   );
   return data;
